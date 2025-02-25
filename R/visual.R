fb16bf8a4ba93a692c88661eb29de6c47116ffd5
qwding101 <r07227101@ntu.edu.tw>
2025-02-25 09:50:22 +0800

    Debug plot functions

--- R/visual.R
+++ R/visual.R
@@ -1,36 +1,41 @@
-#' Plot individual spectral estimate
+#' Plot individual spectral density estimate
 #'
 #' @description
-#' Use `ggplot2` package to plot the heatmap of a single periodogram or kernel spectral estimate.
+#' Use `ggplot2` package to plot the heatmap of a single periodogram or kernel
+#' spectral density estimate.
 #'
 #' @param period Matrix. Output from `periodogram()` or [`periodogram_smooth()`].
 #' @param freq.list List. Output from [`generate_freq()`] with `return.comb = TRUE`.
+#' @param type If `type = "Re"` (default), plot the real part of the estimate.
+#' If `type = "Im"`, plot the imaginary part. This argument is useful for cross-
+#' spectrum estimate, which is complex-valued.
 #' @param remove An experimental feature. Please ignore.
 #' @param title Character. The title of the plot.
 #' @param palette Color schemes for visualization. Type `?scale_fill_distiller()`
 #'  to check available options.
-#' @param legend.range Input `c(lower, upper)` to manually set the lower and upper limit of the spectral density to visualize.
+#' @param legend.range Input `c(lower, upper)` to manually set the lower and upper
+#' limit of the spectral density to visualize.
 #' @seealso [`plot_pairs()`]
 #' @examples
 #' library(spatstat)
 #' lam <- function(x, y, m) {(x^2 + y) * ifelse(m == "A", 2, 1)}
+#' set.seed(227823)
 #' spp <- rmpoispp(lambda = lam, win = square(5), types = c("A","B"))
-#' period.list <- periodogram_smooth(spp, inten.formula = "~ x + y", bandwidth = 1.2)
-#' names(period.list)
+#' KSDE.list <- periodogram_smooth(spp, inten.formula = "~ x + y", bandwidth = 1.15)
+#' names(KSDE.list)
 #'
-#' # Only plot the real part of the periodogram
-#' period.list <- lapply(period.list, Re)
-#' freq.list <- generate_freq(A1 = diff(spp$window$xrange), A2 = diff(spp$window$yrange),
-#'                            return.comb = TRUE)
-#' plot_period(period.list[[1]], freq.list = freq.list,
+#' freq.list <- attr(KSDE.list, "freq.list")
+#' plot_period(KSDE.list[[1]], freq.list = freq.list,
 #'             title = "Kernel spectral estimate: type A")
-#' plot_period(period.list[[2]], freq.list = freq.list,
-#'             title = "Kernel spectral estimate: cross-spectrum")
-#' plot_period(period.list[[3]], freq.list = freq.list,
+#' plot_period(KSDE.list[[2]], freq.list = freq.list,
+#'             title = "Kernel spectral estimate: cross-spectrum (Real part)")
+#' plot_period(KSDE.list[[2]], freq.list = freq.list, type = "Im",
+#'             title = "Kernel spectral estimate: cross-spectrum (Imaginary part)")
+#' plot_period(KSDE.list[[3]], freq.list = freq.list,
 #'             title = "Kernel spectral estimate: type B")
 #' @importFrom ggplot2 .data
 #' @export
-plot_period = function(period, freq.list = NULL, remove=NULL,
+plot_period = function(period, freq.list = NULL, type = "Re", remove=NULL,
                        title = NULL, palette = "Spectral", legend.range = NULL){
 
   if (is.matrix(period)){
@@ -39,11 +44,16 @@ plot_period = function(period, freq.list = NULL, remove=NULL,
     }else{
       omega.comb = freq.list$omega.comb
     }
+
+    period = switch(type,
+                    Re = Re(period),
+                    Im = Im(period),
+                    stop("`type` should be 'Re' or 'Im'.", call. = FALSE))
     period.long = as.data.frame(omega.comb)
     period.long$dist = sqrt(period.long$omega1^2 + period.long$omega2^2)
     period.long$Density = as.vector(t(period))
   }
-  else if (is.data.frame(period)){
+  else if (is.data.frame(period)){ # Used for plotting coherence
     period.long = period
   }
   else{
@@ -65,42 +75,48 @@ plot_period = function(period, freq.list = NULL, remove=NULL,
 
 
 
-#' Plot all possible spectral estimates
+#' Plot all possible spectral density estimates
 #'
 #' @description
 #' Given a multivariate (multitype) point pattern, use `ggplot2` package to
-#' visualize all (marginal and joint) kernel spectral estimates.
+#' visualize all (marginal and joint) spectral density estimates.
 #'
-#' @param est.list List. Output from [`periodogram_smooth()`].
+#' @param est.list List. The kernel spectral density estimate from [`periodogram_smooth()`].
 #' @param ppp A point pattern of class `"ppp"`.
 #' @param xnorm Logical. If `TRUE` (default), plot the radially-averaged spectral
 #' estimates. Otherwise, plot the raw values by heatmap.
-#' @param freq.list List. Output from [`generate_freq()`] with `return.comb = TRUE`.
+#' @param type If `type = "Re"` (default), plot the real part of the estimates.
+#' If `type = "Im"`, plot the imaginary part.
 #' @param shared.legend Logical. Whether to share the legend across all plots.
 #' @param remove An experimental feature. Please ignore.
 #'
 #' @examples
 #' library(spatstat)
 #' lam <- function(x, y, m) {(x^2 + y) * ifelse(m == "A", 2, 1)}
+#' set.seed(227823)
 #' spp <- rmpoispp(lambda = lam, win = square(5), types = c("A","B"))
-#' period.list <- periodogram_smooth(spp, inten.formula = "~ x + y", bandwidth = 1.2)
+#' KSDE.list <- periodogram_smooth(spp, inten.formula = "~ x + y", bandwidth = 1.15)
 #'
-#' # Only plot the real part of the periodogram
-#' period.list <- lapply(period.list, Re)
-#' freq.list <- generate_freq(A1 = diff(spp$window$xrange), A2 = diff(spp$window$yrange),
-#'                            return.comb = TRUE)
-#' plot_pairs(est.list = period.list, ppp = spp, freq.list = freq.list)
+#' plot_pairs(est.list = KSDE.list, ppp = spp)
+#' plot_pairs(est.list = KSDE.list, ppp = spp, type = "Im")
+#' plot_pairs(est.list = KSDE.list, ppp = spp, xnorm = FALSE)
+#' plot_pairs(est.list = KSDE.list, ppp = spp, xnorm = FALSE, type = "Im")
 #' @importFrom ggplot2 .data
 #' @export
-plot_pairs = function(est.list, ppp, xnorm = TRUE, freq.list = NULL,
+plot_pairs = function(est.list, ppp, xnorm = TRUE, type = "Re",
                       shared.legend = TRUE, remove = NULL){
+
   cate = levels(spatstat.geom::marks(ppp))
   plot.layout = matrix(NA, ncol = length(cate), nrow = length(cate))
   plot.layout[lower.tri(plot.layout, diag = TRUE)] = seq_along(est.list)
-
   titles = rep("", length(plot.layout))
   titles[diag(plot.layout)] = cate
+  freq.list = attr(est.list, "freq.list")
 
+  est.list = switch(type,
+                    Re = lapply(est.list, Re),
+                    Im = lapply(est.list, Im),
+                    stop("`type` should be 'Re' or 'Im'.", call. = FALSE))
   legend.range = switch(shared.legend + 1, # Basically `ifelse()` but ifelse() cannot return NULL
                         NULL,
                         c(min(sapply(est.list, min)),
@@ -111,16 +127,11 @@ plot_pairs = function(est.list, ppp, xnorm = TRUE, freq.list = NULL,
 
 
   if (xnorm){
-    if (is.null(freq.list)){
-      omega.comb = attr(est.list, "freq.list")$omega.comb
-    }else{
-      omega.comb = freq.list$omega.comb
-    }
-    period.long = as.data.frame(omega.comb)
+
+    period.long = as.data.frame(freq.list$omega.comb)
     period.long$dist = sqrt(period.long$omega1^2 + period.long$omega2^2)
 
     for (r in seq_along(est.list)){
-
       period.long$Density = as.vector(t(est.list[[r]]))
       period.plot.li[[r]] = dplyr::group_by(period.long, .data$dist) %>%
         dplyr::summarise(avg.density = mean(.data$Density)) %>%
@@ -143,13 +154,17 @@ plot_pairs = function(est.list, ppp, xnorm = TRUE, freq.list = NULL,
     result = result +
       cowplot::draw_label(expression(paste("||", bold("\u03c9"), "||")),
                           x=0.5, y=0, vjust=-0.5, size=12, angle=0) +
-      cowplot::draw_label("Radially averaged density", x=0, y=0.5, vjust= 1.5,
+      cowplot::draw_label(ifelse(type == "Re",
+                                 paste("Radially averaged density", "(Real part)"),
+                                 paste("Radially averaged density", "(Imaginary part)")),
+                          x=0, y=0.5, vjust= 1.5,
                           size=12, angle=90)
 
   }else{
     for (r in seq_along(est.list)){
       period.plot.li[[r]] = plot_period(est.list[[r]],
-                                        freq.list = attr(est.list, "freq.list"),
+                                        #freq.list = attr(est.list, "freq.list"),
+                                        freq.list = freq.list,
                                         remove = remove,
                                         title = names(est.list)[r],
                                         legend.range = legend.range) +
@@ -157,9 +172,10 @@ plot_pairs = function(est.list, ppp, xnorm = TRUE, freq.list = NULL,
         ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
     }
     if (shared.legend){
+      # common.legend = cowplot::get_legend(period.plot.li[[1]])
+      # Above code can work but pops out unnecessary warning message
+      common.legend = cowplot::get_plot_component(period.plot.li[[1]], "guide-box-right")
       period.plot.li = lapply(period.plot.li, function(x) x + ggplot2::guides(fill = "none"))
-      common.legend = cowplot::get_legend(plot_period(est.list[[1]],
-                                                      legend.range = legend.range))
       result = cowplot::plot_grid(plotlist = period.plot.li[as.vector(plot.layout)],
                                   nrow = length(cate), ncol = length(cate),
                                   byrow = F, scale = 0.85)
@@ -186,26 +202,25 @@ plot_pairs = function(est.list, ppp, xnorm = TRUE, freq.list = NULL,
 #' @description
 #' Use `ggplot2` package to visualize the coherence and partial coherence.
 #'
-#' @param sp.est List. The kernel spectral estimate from [`periodogram_smooth()`].
+#' @param sp.est List. The kernel spectral density estimate from [`periodogram_smooth()`].
 #' @param coh.mat Coherence matrix from [`coherence()`] with `type = "normal"`.
 #' @param partial.coh.mat Partial coherence matrix from [`coherence()`] with `type = "partial"`.
 #' @param xnorm Logical. If `TRUE` (default), plot the radial averaged values.
 #' If `FALSE`, plot the raw coherence and partial coherence values via heatmap.
-#' @param ylim A numeric vector `c(lower, upper)` to specify the range to draw for the
-#' radially-averaged plot. Not required if `xnorm = FALSE`.
+#' @param ylim A numeric vector `c(lower, upper)` to specify the range to draw
+#' for the radially-averaged plot. Not required if `xnorm = FALSE`.
 #'
 #' @seealso [`coherence()`]
 #' @examples
 #' library(spatstat)
 #' lam <- function(x, y, m) {(x^2 + y) * ifelse(m == "A", 2, 1)}
+#' set.seed(227823)
 #' spp <- rmpoispp(lambda = lam, win = square(5), types = c("A","B"))
 #'
-#' # Compute kernel spectral estimator with fitted intensity by log-linear model: HERE
-#' # with Cartesian coordinates
-#' spectra <- periodogram_smooth(spp, inten.formula = "~ x + y", bandwidth = 1.2)
-#' coh.partial <- coherence(spectra, spp)
-#' coh <- coherence(spectra, spp, type = "normal")
-#' plot_coher(spectra, coh, coh.partial)
+#' KSDE.list <- periodogram_smooth(spp, inten.formula = "~ x + y", bandwidth = 1.15)
+#' coh.partial <- coherence(KSDE.list, spp)
+#' coh <- coherence(KSDE.list, spp, type = "normal")
+#' plot_coher(KSDE.list, coh, coh.partial)
 #' @importFrom ggplot2 .data
 #' @export
 plot_coher = function(sp.est, coh.mat, partial.coh.mat, xnorm = TRUE, ylim = NULL){
