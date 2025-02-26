5533a2df61e2d249492c9926a27f02b04d270eef
qwding101 <r07227101@ntu.edu.tw>
2025-02-26 16:39:25 +0800

    Use latex

--- R/H.h.lambda.1.R
+++ R/H.h.lambda.1.R
@@ -1,12 +1,19 @@
-#' Compute H.h.lambda.1 in the centered DFT
+#' Compute \eqn{\hat{H}^{(n)}_{h\hat{\lambda}^{(i)},1}} in the centered DFT
+#'
+#' @description
+#' This function computes \deqn{\hat{H}^{(n)}_{h\hat{\lambda}^{(i)},1}(\boldsymbol\omega) =
+#' \int_{D_n}h(\boldsymbol{x}/\boldsymbol{A})\hat{\lambda}^{(i)}(\boldsymbol{x})
+#' \exp(-i\boldsymbol{x}^\intercal\boldsymbol\omega)d\boldsymbol{x}}
+#' for the \eqn{i}th point process, \eqn{i\in\{1,2,\ldots,m\}}.
 #'
 #' @param w1,w2 A numeric value or vector of frequency values at horizontal and
 #' vertical directions, respectively.
 #' @param a Taper coefficient, a value within unit interval. If `a = 1`, then
-#'  taper is not applied.
-#' @param taper Data taper function.
+#'  taper is not applied, i.e., \eqn{h(\boldsymbol{x}/\boldsymbol{A}) = 1}.
+#' @param taper Data taper function \eqn{h}.
 #' @param A1,A2 Side lengths of the observation window.
-#' @param inten.fitted Intensity function of individual point pattern.
+#' @param inten.fitted Fitted intensity function of individual point pattern,
+#' \eqn{\hat{\lambda}^{(i)}(\cdot)}.
 
 H.h.lambda.1 = Vectorize(function(w2, w1, a, taper, A1, A2, inten.fitted){
   surface = spatstat.geom::as.im(X = function(x,y,a,A1,A2,w1,w2) taper(x,a)*taper(y,a)*inten.fitted(x*A1,y*A2)*exp(-1i*(A1*x*w1+A2*y*w2)),
